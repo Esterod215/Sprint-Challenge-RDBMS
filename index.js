9937460adfdd76a5c8e5e27eaae40a4dac8fcd49
Estevan <estevan.rodriguez10@yahoo.com>
2019-02-22 08:14:49 -0800

    finished boilerplatting application

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+
+const port = 3000;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
