0dcc9680854f7208ac63da27d93f7ada8c5b255a
Estevan <estevan.rodriguez10@yahoo.com>
2019-02-22 09:53:25 -0800

    Finished the get request where it returns both the project and the action but not in the approrpiate format. Also finished implementing the rest of the crud operations for both projects and actions.

--- index.js
+++ index.js
@@ -39,6 +39,34 @@ server.get('/api/projects', async(req,res)=>{
       }
   });
 
+  server.put('/api/projects/:id',async(req,res)=>{
+    try{
+      const count = await db('projects').where({id:req.params.id}).update(req.body);
+      if(count>0){
+        const project = await db('projects').where({ id:req.params.id}).first();
+      res.status(200).json(project)
+      }else{
+        res.status(404).json('Project Not Found')
+      }
+    }catch(error){}
+  });
+  
+  server.delete('/api/projects/:id',async(req,res)=>{
+    try{
+      const count = await db('projects')
+      .where({id:req.params.id})
+      .del();
+      
+      if(count > 0){
+      res.status(204).end();
+      }else{
+        res.status(404).json('Cohort Not Found')
+      }
+    }catch(error){}
+  });
+
+  
+  
   server.post('/api/actions',async(req,res)=>{
     try{
       const [id]= await db('actions').insert(req.body);
@@ -47,6 +75,47 @@ server.get('/api/projects', async(req,res)=>{
     }catch(error){
       res.status(500).json(error);
       }
+  });
+  server.put('/api/actions/:id',async(req,res)=>{
+    try{
+      const count = await db('actions').where({id:req.params.id}).update(req.body);
+      if(count>0){
+        const project = await db('actions').where({ id:req.params.id}).first();
+      res.status(200).json(project)
+      }else{
+        res.status(404).json('action Not Found')
+      }
+    }catch(error){}
+  });
+  
+  server.delete('/api/actions/:id',async(req,res)=>{
+    try{
+      const count = await db('actions')
+      .where({id:req.params.id})
+      .del();
+      
+      if(count > 0){
+      res.status(204).end();
+      }else{
+        res.status(404).json('action Not Found')
+      }
+    }catch(error){}
+  });
+
+  
+  
+  server.get('/api/project-actions', async(req,res)=>{
+    try{
+      
+        const actions = await db('actions').join('projects', 'projects.id', 'actions.project_id')
+        .select('projects.id AS project-id','projects.name','projects.description AS project-description'
+        ,'actions.id AS actions-id','actions.description','actions.notes','actions.project_id');
+      
+
+      res.status(200).json(actions);
+    }catch(error){
+      res.status(500).json(error);
+    }
   });
 
 
