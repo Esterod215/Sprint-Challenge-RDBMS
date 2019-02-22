6896bed66a10e39b533bf64328a82daaed0e71ec
Estevan <estevan.rodriguez10@yahoo.com>
2019-02-22 09:02:28 -0800

    added two get requests for projects and actions to test if i got data from them individually.Completed post request for projects.

--- index.js
+++ index.js
@@ -11,6 +11,34 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+server.get('/api/projects', async(req,res)=>{
+    try{
+      const projects = await db('projects');
+      res.status(200).json(projects);
+    }catch(error){
+      res.status(500).json(error);
+    }
+  });
+
+  server.get('/api/actions', async(req,res)=>{
+    try{
+      const actions = await db('actions');
+      res.status(200).json(actions);
+    }catch(error){
+      res.status(500).json(error);
+    }
+  });
+
+  server.post('/api/projects',async(req,res)=>{
+    try{
+      const [id]= await db('projects').insert(req.body);
+      const project = await db('projects').where({ id }).first();
+      res.status(201).json(project);
+    }catch(error){
+      res.status(500).json(error);
+      }
+  });
+
 
 const port = 3000;
 server.listen(port, function() {
